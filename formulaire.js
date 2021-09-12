fbd2e9ab367f7ccfe541535407c11f7fe36d96eb
Inès Hammami <ines-melya@hotmail.fr>
2021-09-12 19:14:57 +0200

    deleting comments in js code

--- formulaire.js
+++ formulaire.js
@@ -2,7 +2,7 @@
 (function () {
   'use strict'
 
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  
   var forms = document.querySelectorAll('.needs-validation')
 
 
